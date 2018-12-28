638258f84affce3a09644ef143cec17cc56acf05
mikhail.gubanov <mikhail.gubanov@team.wrike.com>
2018-12-29 01:41:38 +0300

    determinant

--- ru.lsreg.math.test/MatrixTest.cs
+++ ru.lsreg.math.test/MatrixTest.cs
@@ -40,10 +40,78 @@ namespace ru.lsreg.math.test
             testMatrix2[1, 2] = 1;
             var multipliedMatrix = testMatrix * testMatrix2;
             Assert.Equal(multipliedMatrix.M, testMatrix.M);
-            Assert.Equal(multipliedMatrix.N, testMatrix2.N);            
+            Assert.Equal(multipliedMatrix.N, testMatrix2.N);
             TestHelpers.AssertDoubleEqual(multipliedMatrix[0, 0], -0.1);
             TestHelpers.AssertDoubleEqual(multipliedMatrix[0, 1], -1);
             TestHelpers.AssertDoubleEqual(multipliedMatrix[0, 2], 6);
+        }
+
+        [Fact]
+        public void TestMatrixAddition()
+        {
+            var testMatrix = new Matrix(1, 2);
+            testMatrix[0, 0] = 1;
+            testMatrix[0, 1] = 3;
+            var testMatrix2 = new Matrix(1, 2);
+            testMatrix2[0, 0] = 2;
+            testMatrix2[0, 1] = -1;
+            var resultMatrix = testMatrix + testMatrix2;
+            TestHelpers.AssertDoubleEqual(resultMatrix[0, 0], 3);
+            TestHelpers.AssertDoubleEqual(resultMatrix[0, 1], 2);
+        }
+
+        [Fact]
+        public void TestMatrixSubstraction()
+        {
+            var testMatrix = new Matrix(1, 2);
+            testMatrix[0, 0] = 1;
+            testMatrix[0, 1] = 3;
+            var testMatrix2 = new Matrix(1, 2);
+            testMatrix2[0, 0] = 2;
+            testMatrix2[0, 1] = -1;
+            var resultMatrix = testMatrix - testMatrix2;
+            TestHelpers.AssertDoubleEqual(resultMatrix[0, 0], -1);
+            TestHelpers.AssertDoubleEqual(resultMatrix[0, 1], 4);
+        }
+
+        [Fact]
+        public void TestCreateTransposeMatrix()
+        {
+            var testMatrix = new Matrix(1, 2);
+            testMatrix[0, 0] = 1;
+            testMatrix[0, 1] = 3;
+            var transposeMatrix = testMatrix.CreateTransposeMatrix();
+            Assert.Equal(transposeMatrix.M, 2);
+            Assert.Equal(transposeMatrix.N, 1);
+
+            TestHelpers.AssertDoubleEqual(transposeMatrix[0, 0], 1);
+            TestHelpers.AssertDoubleEqual(transposeMatrix[1, 0], 3);
+        }
+
+        [Fact]
+        public void TestMatrixIsSquare()
+        {
+            Assert.Equal(new Matrix(1, 2).IsSquare, false);
+            Assert.Equal(new Matrix(2, 2).IsSquare, true);
+        }
+
+        [Fact]
+        public void TestDeterminant()
+        {
+            var testMatrix = new Matrix(3, 3);
+            testMatrix[0, 0] = 1;
+            testMatrix[0, 1] = 3;
+            testMatrix[0, 2] = -2;
+            testMatrix[1, 0] = 1;
+            testMatrix[1, 1] = 0;
+            testMatrix[1, 2] = 7;
+            testMatrix[2, 0] = 1;
+            testMatrix[2, 1] = 1;
+            testMatrix[2, 2] = -1;
+
+            TestHelpers.AssertDoubleEqual(testMatrix.CalculateDeterminant(), 15);
+            testMatrix[1, 1] = 2;
+            TestHelpers.AssertDoubleEqual(testMatrix.CalculateDeterminant(), 17);
         }
     }
 }
--- ru.lsreg.math/Matrix.cs
+++ ru.lsreg.math/Matrix.cs
@@ -1,62 +1,150 @@
 using System;
 
-public class Matrix {
+public class Matrix
+{
     private double[,] data;
+    private double precalculatedDeterminant = double.NaN;
 
     private int m;
-    public int M {get => this.m;}
-    
+    public int M { get => this.m; }
+
     private int n;
-    public int N {get => this.n;}
-    
+    public int N { get => this.n; }
+
+    public bool IsSquare { get => this.M == this.N; }
+
     public void ProcessFunctionOverData(Action<int, int> func)
     {
-        for(var i = 0; i < this.M; i++)
+        for (var i = 0; i < this.M; i++)
         {
-            for(var j = 0; j < this.N; j++)
+            for (var j = 0; j < this.N; j++)
             {
                 func(i, j);
             }
         }
     }
 
-    public Matrix(int m, int n) 
+    public static Matrix CreateIdentityMatrix(int n)
+    {
+        var result = new Matrix(n, n);
+        for (var i = 0; i < n; i++)
+        {
+            result[i, i] = 1;
+        }
+        return result;
+    }
+
+    public Matrix CreateTransposeMatrix()
+    {
+        var result = new Matrix(this.N, this.M);
+        result.ProcessFunctionOverData((i, j) => result[i, j] = this[j, i]);
+        return result;
+    }
+
+    public Matrix(int m, int n)
     {
         this.m = m;
         this.n = n;
         this.data = new double[m, n];
         this.ProcessFunctionOverData((i, j) => this.data[i, j] = 0);
     }
-    
-    public double this[int x, int y] 
+
+    public double this[int x, int y]
     {
-        get 
+        get
         {
             return this.data[x, y];
         }
-        set {
+        set
+        {
             this.data[x, y] = value;
-        }        
+            this.precalculatedDeterminant = double.NaN;
+        }
     }
 
-    public static Matrix operator* (Matrix matrix, double value) 
+    public double CalculateDeterminant()
     {
-        var result = new Matrix(matrix.M, matrix.N);
-        result.ProcessFunctionOverData((i, j) => result[i,j] = matrix[i,j] * value);
+        if (!double.IsNaN(this.precalculatedDeterminant))
+        {
+            return this.precalculatedDeterminant;
+        }
+        if (!this.IsSquare)
+        {
+            throw new InvalidOperationException("determinant can be calculated only for square matrix");
+        }
+        if (this.N == 2)
+        {
+            return this[0, 0] * this[1, 1] - this[0, 1] * this[1, 0];
+        }
+        double result = 0;
+        for (var j = 0; j < this.N; j++)
+        {
+            result += (j % 2 == 1 ? 1 : -1) * this[1, j] * 
+                this.CreateMatrixWithoutColumn(j).CreateMatrixWithoutRow(1).CalculateDeterminant();
+        }
+        this.precalculatedDeterminant = result;
         return result;
     }
 
-    public static Matrix operator* (Matrix matrix, Matrix matrix2) 
+    private Matrix CreateMatrixWithoutRow(int row)
     {
-        if (matrix.N != matrix2.M) {
+        if (row < 0 || row >= this.M)
+        {
+            throw new ArgumentException("invalid row index");
+        }
+        var result = new Matrix(this.M - 1, this.N);
+        result.ProcessFunctionOverData((i, j) => result[i, j] = i < row ? this[i, j] : this[i + 1, j]);
+        return result;
+    }
+
+    private Matrix CreateMatrixWithoutColumn(int column)
+    {
+        if (column < 0 || column >= this.N)
+        {
+            throw new ArgumentException("invalid column index");
+        }
+        var result = new Matrix(this.M, this.N - 1);
+        result.ProcessFunctionOverData((i, j) => result[i, j] = j < column ? this[i, j] : this[i, j + 1]);
+        return result;
+    }
+
+    public static Matrix operator *(Matrix matrix, double value)
+    {
+        var result = new Matrix(matrix.M, matrix.N);
+        result.ProcessFunctionOverData((i, j) => result[i, j] = matrix[i, j] * value);
+        return result;
+    }
+
+    public static Matrix operator *(Matrix matrix, Matrix matrix2)
+    {
+        if (matrix.N != matrix2.M)
+        {
             throw new ArgumentException("matrixes can not be multiplied");
         }
         var result = new Matrix(matrix.M, matrix2.N);
-        result.ProcessFunctionOverData((i, j) => {
-            for(var k = 0; k < matrix.N; k++) {
+        result.ProcessFunctionOverData((i, j) =>
+        {
+            for (var k = 0; k < matrix.N; k++)
+            {
                 result[i, j] += matrix[i, k] * matrix2[k, j];
             }
         });
         return result;
+    }
+
+    public static Matrix operator +(Matrix matrix, Matrix matrix2)
+    {
+        if (matrix.M != matrix2.M || matrix.N != matrix2.N)
+        {
+            throw new ArgumentException("matrixes dimensions should be equal");
+        }
+        var result = new Matrix(matrix.M, matrix.N);
+        result.ProcessFunctionOverData((i, j) => result[i, j] = matrix[i, j] + matrix2[i, j]);
+        return result;
+    }
+
+    public static Matrix operator -(Matrix matrix, Matrix matrix2)
+    {
+        return matrix + (matrix2 * -1);
     }
 }
